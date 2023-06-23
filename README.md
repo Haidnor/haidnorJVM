f5fa3f35ed56b6c564ce5d2e4c0439762015d9e2
wangxiang <276644985@qq.com>
2023-06-23 09:48:11 +0800

    update

--- README.md
+++ README.md
@@ -1,20 +1,2 @@
-# haidnorVM
-一个用 Java 编写的元循环虚拟机
-
-# 编译方式
-打开窗口:
-```cmd
-C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Visual Studio 2022\Visual Studio Tools\VC\x64 Native Tools Command Prompt for VS 2022
-```
-进入项目文件夹
-```
-cd \java_project\haidnorVM
-```
-执行编译
-```cmd
-mvn clean package
-```
-
-字节码规范文档
-https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html
-
+bytecode 分支内容:
+自己编码解析字节码文件
--- src/test/java/haidnor/vm/ClassLoaderTest.java
+++ src/test/java/haidnor/vm/ClassLoaderTest.java
@@ -2,33 +2,17 @@ package haidnor.vm;
 
 import haindor.vm.bytecode.ClassFile;
 import haindor.vm.classloader.ClassLoader;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.bcel.classfile.ClassParser;
-import org.apache.bcel.classfile.JavaClass;
 import org.junit.Test;
 
-@Slf4j
 public class ClassLoaderTest {
 
     @Test
-    public void test_() throws Exception {
+    public void test() throws Exception {
         String path = "D:\\java_project\\JavaClassTest\\out\\production\\JavaClassTest\\Main.class";
 
         ClassLoader classLoader = new ClassLoader();
         ClassFile classFile = classLoader.readClassFile(path);
         System.out.println(classFile);
-    }
-
-
-    @Test
-    public void test_1() throws Exception {
-//        Repository.setRepository(Repository.getRepository());
-//        ClassPath.ClassFile classFile = Repository.lookupClassFile("D:\\java_project\\JavaClassTest\\out\\production\\JavaClassTest\\Main.class");
-//        System.out.println(classFile);
-
-        ClassParser classParser = new ClassParser("D:\\java_project\\JavaClassTest\\out\\production\\JavaClassTest\\Main.class");
-        JavaClass parse = classParser.parse();
-        System.out.println(parse);
     }
 
 }
